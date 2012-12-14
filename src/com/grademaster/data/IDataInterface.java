0f68d98620f59f7f2397f14d4dc325f718ed8132
eakjb <jake@eakjb.com>
2012-12-13 18:36:26 -0700

    Added IDataInterface and setup for Java Dev.
    Trying commit again

---
+++ src/com/grademaster/data/IDataInterface.java
@@ -0,0 +1,27 @@
+package com.grademaster.data;
+import java.io.Flushable;
+import java.io.IOException;
+import java.util.HashMap;
+
+//Author: Jake Billings
+//Date created: 12-13-2012
+//Desc: provides an interface for base methods for data io.
+
+public interface IDataInterface extends Flushable {
+	//Path should always be different, so it is path error here.
+	public String path = "PathError";
+	//True=Caching on; False = caching off
+	public boolean cached = true;
+	// Loads data from file.  DOES NOT UPDATE CACHE; DOES NOT USE CACHE
+	public HashMap<Object,Object> loadData();
+	// Loads data using and/or updating cache.
+	public HashMap<Object,Object> getData();
+	// Updates cache with given data
+	public void updateCache(HashMap<Object,Object> data);
+	// Writes the contents of the current cache to the data source.
+	public void dumpData() throws IOException;
+	// Writes updates cache with given data, then writes the data
+	public void dumpData(HashMap<Object,Object> data) throws IOException;
+	// Writes provided data to data source; DOES NOT UPDATE CACHE
+	public void writeData(HashMap<Object,Object> data) throws IOException;
+}
